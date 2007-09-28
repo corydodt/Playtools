f19bebf8f4260ab2e420aaf8d3aa00e1c8d6fae7
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-09-27 21:48:49 -0700

    add rdfName to util and test it there too

---
+++ playtools/test/test_util.py
@@ -0,0 +1,18 @@
+
+from twisted.trial import unittest
+
+from playtools import util
+
+class UtilTestCase(unittest.TestCase):
+
+    def test_rdfName(self):
+        """
+        Test ability to convert weird names into rdf strings in a standard way
+        """
+        s1 = "thing"
+        s2 = "The Thing. that (we want)"
+        s3 = "The Thing, that (we want)"
+        self.assertEqual(util.rdfName(s1), "thing")
+        self.assertEqual(util.rdfName(s2), "theThingThatWeWant")
+        self.assertEqual(util.rdfName(s3), "theThingThatWeWant")
+
--- playtools/util.py
+++ playtools/util.py
@@ -1,3 +1,24 @@
+import os
+
 from twisted.python.util import sibpath
 
 RESOURCE = lambda f: sibpath(__file__, f)
+
+def rdfName(s):
+    """Return a string suitable for an IRI from s"""
+    s = s.replace('.', ' ')
+    s = s.replace('-', ' ')
+    s = s.replace("'", '')
+    s = s.replace('/', ' ')
+    s = s.replace(':', ' ')
+    s = s.replace(',', ' ')
+    s = s.replace('(', ' ').replace(")", ' ')
+    s = s.replace('[', ' ').replace("]", ' ')
+    parts = s.split()
+    parts[0] = parts[0].lower()
+    parts[1:] = [p.capitalize() for p in parts[1:]]
+    return ''.join(parts)
+
+def filenameAsUri(fn):
+    return 'file://' + os.path.abspath(fn)
+
