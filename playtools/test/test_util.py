c036933498966a0ce9188a109bd5bfd455cabcbb
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2007-10-01 12:01:35 -0700

    test_filenameAsUri

--- playtools/test/test_util.py
+++ playtools/test/test_util.py
@@ -1,5 +1,7 @@
+import os
 
 from twisted.trial import unittest
+from twisted.python.util import sibpath
 
 from playtools import util
 
@@ -16,3 +18,11 @@ class UtilTestCase(unittest.TestCase):
         self.assertEqual(util.rdfName(s2), "theThingThatWeWant")
         self.assertEqual(util.rdfName(s3), "theThingThatWeWant")
 
+    def test_filenameAsUri(self):
+        """
+        filenameAsUri should accept only strings and return file:// uris
+        """
+        self.assertRaises(Exception, util.filenameAsUri, 2)
+        here = lambda f: os.getcwd() + '/' + f
+        self.assertEqual(util.filenameAsUri('x'), 'file://%s' % (here('x'),))
+        self.assertEqual(util.filenameAsUri('/x/y/z'), 'file:///x/y/z')
