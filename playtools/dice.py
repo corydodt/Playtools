14352d4a438c83c7ffc514aa0ce294be521c1299
Cory Dodt <corymercurial@spam.goonmill.org>
2009-02-02 19:47:30 -0800

    tested dice roller

---
+++ playtools/dice.py
@@ -0,0 +1,152 @@
+"""
+Implement dice rolling using parsed dice
+"""
+import sys
+import random
+
+from playtools import diceparser
+
+def rollDie(die, mod=0):
+    return random.choice(range(1, die+1)) + mod
+
+def parseRange(odds):
+    """parseRange("2-15") => (2, 15) (as ints)
+    Also parseRange("2") => (2, 2)
+    """
+    if '-' in odds:
+        low,hi = odds.split('-')
+    else:
+        low = hi = odds
+    return int(low), int(hi)
+
+def choosePercentile(percentiles):
+    """Utility function for choosing an item from a list formatted like this:
+    ['01-11','12-73','74-99','100']
+    Returns the index of the item selected from the list.
+    """
+    pctile = roll(100)
+    for n, outcome in enumerate(percentiles):
+        low,hi = parseRange(outcome)
+        if low <= pctile <= hi:
+            return n
+    raise RuntimeError, "None of %s were selected" % (percentiles,)
+
+
+def parse(st):
+    """The results of rolling this dice expression, as a list of sums"""
+    parsed = diceparser.parseDice(st)
+    rolled = roll(parsed)
+    return list(rolled)
+
+def roll(parsed, temp_modifier=0):
+    # set these to defaults in the finish step, not in the init, 
+    # so the parser instance can be reused
+    expr = parsed[0]
+    count = expr.count
+    if expr.filterCount is not None:
+        dice_filter_count = expr.filterCount
+        if dice_filter_count > count:
+            _m = "Hi/Lo filter uses more dice than are being rolled"
+            raise RuntimeError(_m)
+
+        hilo = str(expr.filterDirection)
+    else:
+        dice_filter_count = 0
+        hilo = None
+
+    dice_repeat = expr.repeat
+
+    dice_bonus = expr.dieModifier
+
+    if expr.staticNumber is not None:
+        # an int by itself is just an int.
+        for n in xrange(dice_repeat):
+            yield DiceResult([expr.staticNumber], dice_bonus, temp_modifier)
+        return
+
+    for n in xrange(dice_repeat):
+        dierolls = []
+        for n in xrange(count):
+            dierolls.append(rollDie(expr.dieSize, 0))
+        result = DiceResult(dierolls, 
+                            dice_bonus, 
+                            temp_modifier,
+                            hilo, 
+                            dice_filter_count)
+        yield result
+
+class DiceResult:
+    """Representation of all the values rolled by a dice expression."""
+    def __init__(self, dierolls, bonus, temp_modifier=0, filterdirection=None, 
+                 filtercount=0):
+        self.dierolls = dierolls
+        self.bonus = bonus
+        if temp_modifier:
+            self.temp_modifier = temp_modifier
+        else:
+            self.temp_modifier = 0
+           
+        self.filterdirection = filterdirection
+        self.filtercount = filtercount
+
+    def __repr__(self):
+        return 'DiceResult(=%s)' % self.sum()
+
+    def format(self):
+        """Print the sum of the individual (filtered) dice and bonus"""
+        dierolls = self.filtered()[:]
+        if self.bonus:
+            dierolls.append(self.bonus)
+        if self.temp_modifier:
+            dierolls.append(self.temp_modifier)
+
+        if len(dierolls) > 1:
+            formatted_sum = '+'.join(map(str, dierolls))
+            return '%s = %s' % (formatted_sum, self.sum())
+        else:
+            return str(self.sum())
+
+    def filtered(self):
+        if self.filterdirection is None:
+            filter = lambda: self.dierolls
+        elif self.filterdirection in 'hH':
+            filter = self.most
+        elif self.filterdirection in 'lL':
+            filter = self.least
+        return filter()
+
+    def sum(self):
+        return sum(self.filtered()) + self.bonus + self.temp_modifier
+
+    def __cmp__(self, other):
+        return cmp(self.sum(), other.sum())
+
+    def most(self, direction=1):
+        """Return the greatest <count> items in lst"""
+        lst = self.dierolls[:]
+        lst.sort()
+        if direction == 1:
+            lst.reverse()
+        return lst[:self.filtercount]
+
+    def least(self):
+        return self.most(0)
+
+
+def run(argv=None):
+    if argv is None:
+        argv = sys.argv
+    while 1:
+        try:
+            st = raw_input("Roll: ")
+            rolled = [r.sum() for r in parse(st)]
+            if len(list(rolled)) > 1:
+                print "Unsorted--", rolled
+                rolled.sort()
+                rolled.reverse()
+                print "Sorted----", rolled
+            else:
+                print rolled[0]
+        except RuntimeError, e:
+            print e
+
---
+++ playtools/test/test_dice.py
@@ -0,0 +1,37 @@
+
+import unittest
+
+from .. import dice
+from .. import diceparser
+
+class DiceTestCase(unittest.TestCase):
+    def test_roll(self):
+        self.assertEqual(dice.roll(diceparser.parseDice('5'), -1).next().sum(), 4)
+        self.assertEqual(dice.roll(diceparser.parseDice('5d1'), 1).next().sum(), 6)
+
+    def test_parse(self):
+        for d in [
+                'd6xz',                   # repeat not a number
+                '1d',                     # left out die size
+                '1d6l3l3', '1d6h3l3',     # can't specify more than one filter
+                '1d6h+1',                 # can't leave the die count out of the filter
+                '1d6h2+1',                # can't keep more dice than you started with
+                '',                       # empty should be an error
+                '1d6+5 1d1',              # multiple expressions
+                ]:
+            self.assertRaises(RuntimeError, dice.parse, d)
+
+        for n in xrange(30):
+            self.assertEqual(dice.parse('5')[0].sum(),              5)
+            self.assertEqual(dice.parse('5x3')[2].sum(),            5)
+            self.assertEqual(dice.parse('5+1x3')[2].sum(),          6)
+            self.assertTrue(1  <= dice.parse('d6')[0].sum()         <= 6)
+            self.assertTrue(3  <= dice.parse('3d6')[0].sum()        <= 18)
+            self.assertTrue(1  <= dice.parse('1d  6')[0].sum()      <= 6)
+            self.assertTrue(3  <= dice.parse('d6+2')[0].sum()       <= 8)
+            self.assertTrue(-1 <= dice.parse('d 6 -2')[0].sum()     <= 4)
+            self.assertTrue(4  <= dice.parse('2d6+ 2')[0].sum()     <= 14)
+            self.assertTrue(0  <= dice.parse('2d6-2')[0].sum()      <= 10)
+            self.assertTrue(4  <= dice.parse('9d6h3+1x2')[0].sum()  <= 19)
+            self.assertTrue(2  <= dice.parse('9d6L3-1x2')[0].sum()  <= 17)
+
