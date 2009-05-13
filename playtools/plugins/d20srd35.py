bccdbedfb352aa676272d4b1eb39abdb55ebbd72
Cory Dodt <corymercurial@spam.goonmill.org>
2009-05-13 15:49:09 -0700

    merge rdfquery and d20srd35

--- playtools/plugins/d20srd35.py
+++ playtools/plugins/d20srd35.py
@@ -6,13 +6,30 @@ import re
 from zope.interface import implements, Interface, Attribute
 
 from twisted.plugin import IPlugin
+from twisted.python import log
+
 from storm import locals as SL
 
 from playtools.interfaces import (IRuleSystem, IRuleFact, IRuleCollection,
     IIndexable)
 from playtools.util import RESOURCE
-from playtools import globalRegistry
+from playtools import globalRegistry, sparqly as S
 from playtools.search import textFromHtml
+
+from rdflib.Namespace import Namespace as NS
+from rdflib import RDF
+
+from rdfalchemy import rdfSingle, rdfMultiple
+from rdfalchemy.orm import mapper
+
+
+FAM = NS('http://goonmill.org/2007/family.n3#')
+CHAR = NS('http://goonmill.org/2007/characteristic.n3#')
+DICE = NS('http://goonmill.org/2007/dice.n3#')
+PCCLASS = NS('http://goonmill.org/2007/pcclass.n3#')
+PROP = NS('http://goonmill.org/2007/property.n3#')
+SKILL = NS('http://goonmill.org/2007/skill.n3#')
+FEAT = NS('http://goonmill.org/2007/feat.n3#')
 
 
 class D20SRD35System(object):
@@ -31,8 +48,13 @@ class D20SRD35System(object):
 d20srd35 = D20SRD35System()
 
 
-# the d20srd database is essentially static and permanent, so just open it here.
+# the d20srd databases are essentially static and permanent, so just open them here.
 STORE = SL.Store(SL.create_database('sqlite:' + RESOURCE('plugins/srd35.db')))
+RDFDB = S.TriplesDatabase()
+RDFDB.open(RESOURCE('rdflib.db'))
+# initialize rdfalchemy mapper
+S.rdfsPTClass.db = RDFDB.graph
+
 
 
 class IStormFact(Interface):
@@ -242,4 +264,146 @@ def srdReferenceURL(item):
     base, rule = mapper[item.reference]
     return base % (rule(item.name),)
 
+
+class SpecialArmorClass(S.rdfsPTClass):
+    """Permanent, racial modifier to armor class"""
+    rdf_type = CHAR.SpecialArmorClass
+
+
+class Aura(S.rdfsPTClass):
+    """
+    Permanent effect that extends some distance around the body of the
+    creature.
+    """
+    rdf_type = CHAR.Aura
+
+
+class SpecialAction(S.rdfsPTClass):
+    """Something a creature can do besides attack"""
+    rdf_type = CHAR.SpecialAction
+
+
+class AttackEffect(S.rdfsPTClass):
+    """Some type of damage such as cold or non-magical"""
+    rdf_type = CHAR.AttackEffect
+
+
+class Resistance(S.rdfsPTClass):
+    """A resistance possessed by monsters"""
+    rdf_type = CHAR.Resistance
+
+    attackEffect = rdfSingle(PROP.attackEffect, range_type=CHAR.AttackEffect)
+    value = rdfSingle(RDF.value)
+
+    ## def __repr__(self):
+    ##     return '<%s to %s>' % (self.__class__.__name__, self.attackEffect.label)
+
+
+class Vulnerability(S.rdfsPTClass):
+    """A vulnerability possessed by monsters"""
+    rdf_type = CHAR.Vulnerability
+
+
+class Immunity(S.rdfsPTClass):
+    """An immunity possessed by monsters"""
+    rdf_type = CHAR.Immunity
+
+
+class Sense(S.rdfsPTClass):
+    """A notable sense possessed by monsters, such as darkvision"""
+    rdf_type = CHAR.Sense
+    range = rdfSingle(PROP.range)
+
+
+class Language(S.rdfsPTClass):
+    """A language understood by monsters"""
+    rdf_type = CHAR.Language
+
+
+class SpecialAbility(S.rdfsPTClass):
+    """A notable ability of any kind that isn't a standard combat mechanic"""
+    rdf_type = CHAR.SpecialAbility
+
+
+class SpecialQuality(S.rdfsPTClass):
+    """A notable quality possessed by the creature that is always on"""
+    rdf_type = CHAR.SpecialQuality
+
+
+class CombatMechanic(S.rdfsPTClass):
+    """A special combat mechanic that applies to this creature"""
+    rdf_type = CHAR.CombatMechanic
+
+
+class MoveMechanic(S.rdfsPTClass):
+    """A special move mechanic that applies to this creature"""
+    rdf_type = CHAR.MoveMechanic
+
+
+class Family(S.rdfsPTClass):
+    """A family of monster with shared characteristics"""
+    rdf_type = CHAR.Family
+    senses = rdfMultiple(PROP.sense, range_type=CHAR.Sense)
+    languages = rdfMultiple(PROP.language, range_type=CHAR.Language)
+    immunities = rdfMultiple(PROP.immunity, range_type=CHAR.Immunity)
+    resistances = rdfMultiple(PROP.resistance, range_type=CHAR.Resistance)
+    vulnerabilities = rdfMultiple(PROP.vulnerability,
+            range_type=CHAR.Vulnerability)
+    specialAbilities = rdfMultiple(PROP.specialAbility,
+            range_type=CHAR.SpecialAbility)
+    specialQualities = rdfMultiple(PROP.specialQuality,
+            range_type=CHAR.SpecialQuality)
+    combatMechanics = rdfMultiple(PROP.combatMechanic,
+            range_type=CHAR.CombatMechanic)
+
+
+class Ability(S.rdfsPTClass):
+    """An ability score"""
+    rdf_type = CHAR.AbilityScore
+
+
+class SkillSynergy(S.rdfsPTClass):
+    """A skill synergy"""
+    rdf_type = CHAR.SkillSynergy
+
+    bonus = rdfSingle(PROP.bonus)
+    synergyComment = rdfSingle(PROP.synergyComment)
+    otherSkill = rdfSingle(PROP.fromSkill, range_type=CHAR.Skill)
+
+
+class Skill(S.rdfsPTClass):
+    """A skill usable by monsters, such as Diplomacy"""
+    rdf_type = CHAR.Skill
+
+    keyAbility = rdfSingle(PROP.keyAbility, range_type=CHAR.AbilityScore)
+    synergy = rdfMultiple(PROP.synergy, range_type=CHAR.SkillSynergy)
+    additional = rdfSingle(PROP.additional)
+    epicUse = rdfSingle(PROP.epicUse)
+    skillAction = rdfSingle(PROP.skillAction)
+    skillCheck = rdfSingle(PROP.skillCheck)
+    tryAgainComment = rdfSingle(PROP.tryAgainComment)
+    untrained = rdfSingle(PROP.untrained)
+
+
+class Feat(S.rdfsPTClass):
+    """A feat usable by monsters, such as Weapon Focus"""
+    rdf_type = CHAR.Feat
+    stackable = S.rdfIsInstance(CHAR.StackableFeat)
+    canTakeMultiple = S.rdfIsInstance(CHAR.CanTakeMultipleFeat)
+    epic = S.rdfIsInstance(CHAR.EpicFeat)
+    psionic = S.rdfIsInstance(CHAR.PsionicFeat)
+    isArmorClassFeat = S.rdfIsInstance(CHAR.ArmorClassFeat)
+    isAttackOptionFeat = S.rdfIsInstance(CHAR.AttackOptionFeat)
+    isSpecialActionFeat = S.rdfIsInstance(CHAR.SpecialActionFeat)
+    isRangedAttackFeat = S.rdfIsInstance(CHAR.RangedAttackFeat)
+    isSpeedFeat = S.rdfIsInstance(CHAR.SpeedFeat)
+
+    additional = rdfSingle(PROP.additional)
+    benefit = rdfSingle(PROP.benefit)
+    choiceText = rdfSingle(PROP.choiceText)
+    prerequisiteText = rdfSingle(PROP.prerequisiteText)
+    noFeatComment = rdfSingle(PROP.noFeatComment)
+
+
+mapper()
 
--- playtools/test/test_d20srd35.py
+++ playtools/test/test_d20srd35.py
@@ -3,8 +3,15 @@ We shall be able to pull facts about the srd35 system out using the API
 """
 
 import unittest
+import string
+import re
+
 from .. import fact
 from playtools.plugins import d20srd35
+
+import warnings
+warnings.filterwarnings('ignore', category=SyntaxWarning)
+
 
 class SRD35TestCase(unittest.TestCase):
     """
@@ -65,3 +72,90 @@ class SRD35TestCase(unittest.TestCase):
         """
         self.assertTrue('spell' in self.srd.facts)
         self.assertTrue('monster' in self.srd.facts)
+
+    def test_schemaAccess(self):
+        """
+        I can access the schema at all
+        """
+        fireResistance = d20srd35.Immunity(d20srd35.CHAR.fire)
+
+    def test_feat(self):
+        """
+        Verify that attributes of the feat triples are accessible
+        """
+        heavy = feat('armorProficiencyHeavy')
+
+        exp = '.*Armor Proficiency \(medium\).*'
+        act = heavy.prerequisiteText
+
+        self.failIfEqual(re.match(exp, str(act)), None, "%s != %s" % (exp, act))
+        self.assertEqual(heavy.stackable, False)
+
+    def test_skill(self):
+        """
+        Verify that attributes of the skill triples are accessible
+        """
+        cha = d20srd35.Ability(d20srd35.CHAR.cha)
+        self.assertEqual(str(cha.label), 'Cha')
+
+        dipl = skill('diplomacy')
+
+        self.assertEqual(str(dipl.keyAbility.label), 'Cha')
+        # TODO - should be able to get an integer here straight from the API,
+        # oh well
+        self.assertEqual(int(dipl.synergy[0].bonus), 2)
+
+    def test_family(self):
+        """
+        Verify that attributes of the family triples are accessible
+        """
+        F = family
+        self.assertEqual(len(F('devil').languages), 3)
+
+        mechs = sorted(F('ooze').combatMechanics)
+        self.assertEqual(str(mechs[0].label), 'Not subject to critical hits')
+
+        sqs = sorted(F('undead').specialQualities)
+        self.assertEqual(str(sqs[0].comment), 
+            'Cannot be raised or resurrected.')
+        self.assertEqual(len(F('construct').combatMechanics), 8)
+
+        senses = sorted(F('fey').senses)
+        self.assertEqual(str(senses[0].label), 'Low-Light Vision')
+
+    def test_resistancesAndImmunities(self):
+        """
+        I can read off resistances and immunities from a family
+        """
+        angel = family('angel')
+
+        resl = sorted(pluck(angel.resistances, 'attackEffect', 'label'))
+        resv = sorted(pluck(angel.resistances, 'value'))
+        self.assertEqual(resl, ['Electricity', 'Fire'])
+        self.assertEqual(resv, [10, 10])
+
+        imml = sorted(pluck(angel.immunities, 'label'))
+        self.assertEqual(imml, ['Acid', 'Cold', 'Petrification'])
+
+
+def skill(k):
+    return d20srd35.Skill(getattr(d20srd35.SKILL, k))
+
+def feat(k):
+    return d20srd35.Feat(getattr(d20srd35.FEAT, k))
+
+def family(k):
+    return d20srd35.Family(getattr(d20srd35.FAM, k))
+
+def pluck(items, *attrs):
+    """
+    For each item return getattr(item, attrs[0]) recursively down to the
+    furthest-right attribute in attrs.
+    """
+    a = attrs[0]
+    rest = attrs[1:]
+    these = (getattr(o,a) for o in items)
+    if len(rest) == 0:
+        return these
+    else:
+        return pluck(these, *rest)
