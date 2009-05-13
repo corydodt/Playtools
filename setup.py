6c07d9090438f3117dd1f2cbef29f817aa7118df
Cory Dodt <corydodt+mercurial_spams@gmail.com>
2009-05-12 18:12:00 -0700

    update setup.py to bump pt version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 from setuptools import setup, find_packages
 setup(name='Playtools',
-      version='0.2.0',
+      version='0.3.0',
       author='Cory Dodt',
       description='Playtools for RPG Software',
       url='http://goonmill.org/playtools/',
