ca7300031aa39fa088be6421d5f692fdbd266c78
Cory Dodt <corymercurial@spam.goonmill.org>
2009-05-17 23:17:18 -0700

    clean up import tests

--- playtools/test/test_IMPORT.py
+++ playtools/test/test_IMPORT.py
@@ -5,29 +5,35 @@ Just import everything
 from twisted.trial import unittest
 
 class IMPORTTestCase(unittest.TestCase):
-    def test_importCommon(self):
-        import playtools.common
-
     def test_importCharsheet(self):
         import playtools.charsheet
 
-    def test_importConvert(self):
-        import playtools.convert
+    def test_importCommon(self):
+        import playtools.common
 
-    def test_importDice(self):
-        import playtools.dice
+    def test_importInterfaces(self):
+        import playtools.interfaces
 
     def test_importDiceparser(self):
         import playtools.diceparser
 
-    def test_importInterfaces(self):
-        import playtools.interfaces
+    def test_importDice(self):
+        import playtools.dice
+
+    def test_importConvert(self):
+        import playtools.convert
 
     def test_importSparqly(self):
         import playtools.sparqly
 
     def test_importUtil(self):
         import playtools.util
+
+    def test_importFact(self):
+        import playtools.fact
+
+    def test_importSearch(self):
+        import playtools.search
 
     def test_importPluginsCharsheet(self):
         import playtools.plugins.charsheet
@@ -40,6 +46,15 @@ class IMPORTTestCase(unittest.TestCase):
 
     def test_importPluginsSkills(self):
         import playtools.plugins.skills
+
+    def test_importPluginsD20SRD35Config(self):
+        import playtools.plugins.d20srd35config
+
+    def test_importPluginsD20SRD35(self):
+        import playtools.plugins.d20srd35
+
+    def test_importPluginsPathfinder10(self):
+        import playtools.plugins.pathfinder10
 
     def test_importPluginsUtil(self):
         import playtools.plugins.util
