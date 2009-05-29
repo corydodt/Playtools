d1055bc710987ed78775532a728b6c81fc51162f
Cory Dodt <corymercurial@spam.goonmill.org>
2009-05-29 13:18:49 -0700

    erase buildout:2

--- playtools/plugins/d20srd35config.py
+++ playtools/plugins/d20srd35config.py
@@ -7,3 +7,24 @@ itself first.
 from playtools.util import RESOURCE
 SQLPATH = RESOURCE('plugins/srd35.db')
 RDFPATH = RESOURCE('plugins/srd35rdf.db')
+
+RDFURIS = ['http://www.w3.org/2000/01/rdf-schema#', 
+           'http://goonmill.org/2007/family.n3#', 
+           'http://goonmill.org/2007/characteristic.n3#', 
+           'http://goonmill.org/2007/property.n3#', 
+           'http://goonmill.org/2007/skill.n3#', 
+           'http://goonmill.org/2007/feat.n3#', 
+           'http://goonmill.org/2009/statblock.n3#', 
+           'http://goonmill.org/2007/specialAbility.n3#', 
+           ]
+
+SQLS =    ['d20srd35-sql/class.sql', 
+           'd20srd35-sql/class_table.sql', 
+           'd20srd35-sql/domain.sql', 
+           'd20srd35-sql/equipment.sql', 
+           'd20srd35-sql/item.sql', 
+           'd20srd35-sql/monster.sql', 
+           'd20srd35-sql/power.sql', 
+           'd20srd35-sql/spell.sql', 
+           'd20srd35-sql/__indexes__.sql', 
+           ]
