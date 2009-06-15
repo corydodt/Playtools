717d833a0bd0f680378b8f63712e8e5f86c9ac12
Cory Dodt <corymercurial@spam.goonmill.org>
2009-06-14 22:07:07 -0700

    make it harder to have circular imports importing fact by deferring fact imports

--- playtools/fact.py
+++ playtools/fact.py
@@ -7,20 +7,39 @@ from playtools import PLUGINMODULE
 
 from twisted.plugin import getPlugins
 
-def getSystems():
+
+class SystemLoader(object):
     """
+    Dictionary-like that defers plugin loading until the first time someone wants a
+    plugin (by using systems[name] syntax, i.e. invoking __getitem__).
+
+    This provides rudimentary protection against circular imports.
+
     Doubly-indexed dict of the game systems we have been able to find through
     the plugin system.  Find by either systems[name] or systems[(name, version)]
     """
+    _cachedPlugins = None
+
+    def __getitem__(self, name):
+        if self._cachedPlugins is None:
+            self._cachedPlugins = getSystems()
+            importRuleCollections(self._cachedPlugins)
+
+        return self._cachedPlugins[name]
+
+    def __contains__(self, name):
+        return name in self._cachedPlugins
+
+
+def getSystems():
     l = list(getPlugins(IRuleSystem, PLUGINMODULE))
     ret = {}
     for system in l:
         ret[(system.name, system.version)] = system
-        # FIXME - ambiguously clobber ret[system.name] sometimes.
+        # FIXME - ambiguously clobber _cachedPlugins[system.name] sometimes.
         ret[system.name] = system
-    return ret
 
-systems = getSystems()
+    return ret
 
 def importRuleCollections(gameSystems):
     """
@@ -31,6 +50,6 @@ def importRuleCollections(gameSystems):
     for collection in l:
         gameSystems[collection.system.name].facts[collection.factName] = collection
 
-importRuleCollections(systems)
+systems = SystemLoader()
 
 all = ['systems']
