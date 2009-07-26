a2c81d6825c8e002ddf4d3a1dd3f4af994c73ea2
Cory Dodt <corymercurial@spam.goonmill.org>
2009-07-26 13:34:22 -0700

    "move obsoleted converters away"

---
+++ playtools/plugins/obsolete/__init__.py
@@ -0,0 +1,5 @@
+"""
+Obsoleted plugins, including some ptconvert converters that were already run.
+Since those conversions are done, we don't want or need to keep the converters
+around any more.
+"""
