99b95ceb39d929674279cf93533600521461bdae
Cory Dodt <corymercurial@spam.goonmill.org>
2009-07-29 10:54:50 -0700

    proper skillparser tests

--- playtools/parser/skillparser.py
+++ playtools/parser/skillparser.py
@@ -116,38 +116,3 @@ class SkillItem(object):
         else:
             return "%s%s%s%s" % (self.skillName, sub, splat, qual)
 
-
-tests = ( # {{{
-"""-
-Speak Language (elven, common)
-Speak Language (any five), Jump +16*
-Concentration +19, Craft or Knowledge (any three) +19, Diplomacy +22, Escape Artist +19, Hide +19, Intimidate +20, Listen +23, Move Silently +19, Sense Motive +19, Spot +23, Use Rope +4 (+6 with bindings)
-Concentration -6, Hide +7, Move Silently +5, Psicraft +7, Ride +5, Spot +3
-Knowledge (psionics) +12, Speak Language (ninjish), Jump +1
-Hide +15, Move Silently +7, Listen +6, Spot +2
-Hide +1000, Listen +5, Spot +5
-Hide +9, Intimidate +12, Knowledge (psionics) +12, Listen +14, Psicraft +12, Search +12, Sense Motive +12, Spot +14* (comma, comma, ninjas)
-Concentration +17, Hide +7, Jump +16, Knowledge (arcana) +12, Knowledge (psionics) +12, Knowledge (the planes) +12, Listen +22, Move Silently +11, Psicraft +12, Search +12, Sense Motive +14, Spot +22
-Concentration +14, Diplomacy +17, Jump +0, Knowledge (any two) +15, Listen +16, Search +15, Sense Motive +16, Spellcraft +15 (+17 scrolls), Spot +16, Survival +4 (+6 following tracks), Tumble +15, Use Magic Device +15 (+17 scrolls)
-Appraise +9, Climb +5, Jump +5, Listen +2, Spot +10
-Climb +3, Jump +3
-Listen +6, Move Silently +4, Spot +6
-Spot +1
-Bluff + 15, Concentration +11 (+15 when manifesting defensively), Hide +14, Listen +14, Move Silently +16
-Jump +16 (or as controlling spirit)
-Climb +38, Knowledge (psionics, arcane) +31, Listen +30, Psicraft +31, Spot +30
-Climb +38 (+39 on a good day)*
-Listen +11, Move Silently +7, Spot +11
-Climb +14*, Listen +6, Move Silently +6, Search +2, Spot +6
-Hide +22, Listen +7, Sense Motive +7, Spot +7
-Climb +12, Jump +20, Listen +7, Spot +8
-Listen +10
-Bluff +10*, Diplomacy +6, Disguise +10*, Intimidate +6, Listen +6, Sense Motive +6, Spot +6
-Climb and Skip +2, Jump +2
-""".splitlines()) # }}}
-
-if __name__ == '__main__':
-    for t in tests:
-        print t
-        parsed = parseSkills(t)
-        print parsed
--- playtools/test/test_skillparser.py
+++ playtools/test/test_skillparser.py
@@ -19,7 +19,7 @@ class SkillParserTest(unittest.TestCase):
         Plain jane Skills can parse
         """
         t1 = "Spot +1"
-        t2 = "Concentration -6, Hide +7, Move Silently +5, Psicraft +7, Ride +5, Spot +3"
+        t2 = "Concentration -6, Hide +7, Move Silently +5, Psicraft +7, Ride +5, Spot +0"
 
         parsed1 = skillparser.parseSkills(t1)[0]
         actual = dict([(i.skillName, i.value) for i in parsed1])
@@ -33,7 +33,7 @@ class SkillParserTest(unittest.TestCase):
                 'Move Silently':5, 
                 'Psicraft':7, 
                 'Ride':5, 
-                'Spot':3}
+                'Spot':0}
         self.assertEqual(actual, expected)
 
     def test_null(self):
@@ -49,37 +49,87 @@ class SkillParserTest(unittest.TestCase):
         """
         Saves with odd whitespace parse ok
         """
-        t = "Str 9, Dex 10, Con - , Int 12, Wis 13, Cha 14"
+        t = "Hide + 15,  Move Silently +7 , Listen +6, Spot +2 "
         parsed = skillparser.parseSkills(t)[0]
-        actual = dict([(k, v.bonus) for k,v in parsed.items()])
-        expected = {'str':9, 'dex':10, 'con':0, 'int':12, 'wis':13, 'cha':14}
+        actual = dict([(i.skillName, i.value) for i in parsed])
+        expected = {'Hide': 15, 'Move Silently': 7, 'Listen': 6, 'Spot': 2}
         self.assertEqual(actual, expected)
 
     def test_splat(self):
         """
         Saves with splats parse ok
         """
-        t = "Str 9*, Dex 10*, Con 11*, Int 12*, Wis 13*, Cha 14*"
-        parsed = skillparser.parseSkills(t)[0]
-        actual = dict([(k, v.bonus) for k,v in parsed.items()])
-        expected = {'str':9, 'dex':10, 'con':11, 'int':12, 'wis':13, 'cha':14}
+        t1 = "Climb +14*"
+        parsed = skillparser.parseSkills(t1)[0]
+        actual = dict([(i.skillName, [i.value, i.splat]) for i in parsed])
+        expected = {"Climb":[14, "*"]}
         self.assertEqual(actual, expected)
 
-        for k in 'str dex con int wis cha'.split():
-            self.assertEqual(parsed[k].splat, "*", k)
+        t2 = "Bluff +10*, Diplomacy +6, Disguise +10*, Intimidate +6, Listen +6"
+        parsed = skillparser.parseSkills(t2)[0]
+        actual = dict([(i.skillName, [i.value, i.splat]) for i in parsed])
+        expected = {"Bluff": [10, "*"], "Diplomacy": [6, None], "Disguise": [10, "*"], 
+                "Intimidate": [6, None], "Listen": [6, None]}
+        self.assertEqual(actual, expected)
 
     def test_qualifier(self):
         """
         Skills with qualifier messages parse ok
         """
-        t = "Str 35, Dex 6, Con -, Int 1 (or as controlling spirit), Wis 11 (or as controlling spirit), Cha 10 (or as controlling spirit)"
-        parsed = skillparser.parseSkills(t)[0]
-        actual = dict([(k, v.bonus) for k,v in parsed.items()])
-        expected = {'str':35, 'dex':6, 'con':0, 'int':1, 'wis':11, 'cha':10}
+        t1 = "Jump +16 (or as controlling spirit)"
+        parsed = skillparser.parseSkills(t1)[0]
+        actual = dict([(i.skillName, [i.value, i.qualifier]) for i in parsed])
+        expected = {'Jump': [16, "or as controlling spirit"]}
         self.assertEqual(actual, expected)
 
-        for stat in ['int', 'wis', 'cha']:
-            self.assertEqual(parsed[stat].qualifier, '(or as controlling spirit)', stat)
+        # qualifiers with commas and shit.
+        # psionics is a subskill, not a qualifier.
+        t2 = "Hide +9, Knowledge (psionics, etc) +12, Spot +14* (comma, comma, ninjas)"
+        parsed = skillparser.parseSkills(t2)[0]
+        actual = dict([(i.skillName, [i.value, i.qualifier]) for i in parsed])
+        expected = {'Hide': [9, None], 
+                'Knowledge': [12, None], 
+                'Spot': [14, 'comma, comma, ninjas']}
+        self.assertEqual(actual, expected)
+
+    def test_speakLanguages(self):
+        """
+        Speak Languages is handled correctly
+        """
+        t1 = "Speak Language (elven, common)"
+        parsed = skillparser.parseSkills(t1)[0]
+        actual = dict([(i.skillName, i.subSkills) for i in parsed])
+        expected = {'Speak Language': 'elven, common'}
+        self.assertEqual(actual, expected)
+
+        t2 = "Speak Language (any five), Jump +16"
+        parsed = skillparser.parseSkills(t2)[0]
+        actual = dict([(i.skillName, i.subSkills) for i in parsed])
+        expected = {'Speak Language': 'any five', 'Jump': None}
+        self.assertEqual(actual, expected)
+
+    def test_subSkills(self):
+        """
+        Subskills are gathered
+        """
+        t1 = "Concentration +17, Knowledge (arcana) +12, Knowledge (psionics) +12, Knowledge (the planes) +12, Listen +22"
+        parsed = skillparser.parseSkills(t1)[0]
+        actual = dict([((i.skillName, i.subSkills), i.value) for i in parsed])
+        expected = {('Concentration',None):17,
+                ('Knowledge','arcana'):12,
+                ('Knowledge','psionics'):12,
+                ('Knowledge','the planes'):12,
+                ('Listen',None):22,
+                }
+        self.assertEqual(actual, expected)
+
+        t2 = "Concentration +17, Knowledge (arcana) +12 (+14 while smoking crack)"
+        parsed = skillparser.parseSkills(t2)[0]
+        actual = dict([((i.skillName, i.subSkills), [i.value, i.qualifier]) for i in parsed])
+        expected = {('Concentration',None):[17, None],
+                ('Knowledge','arcana'):[12, '+14 while smoking crack'],
+                }
+        self.assertEqual(actual, expected)
 
 
 class HUGESkillParserTest(unittest.TestCase):
@@ -98,39 +148,9 @@ class HUGESkillParserTest(unittest.TestCase):
             # *which* monster failed, if one does.
             exp = [monster.name, None]
 
-            stat = monster.Skills
+            stat = monster.skills
 
-            # get('name') as a proxy for checking that the monster actually
-            # loaded ok.
-            act = [monster.name, skillparser.parseSkills(stat) and None]
-            self.assertEqual(exp, act)
+            if stat:
+                act = [monster.name, skillparser.parseSkills(stat) and None]
+                self.assertEqual(exp, act)
 
-tests = ( # {{{
-"""-
-Speak Language (elven, common)
-Speak Language (any five), Jump +16*
-Concentration +19, Craft or Knowledge (any three) +19, Diplomacy +22, Escape Artist +19, Hide +19, Intimidate +20, Listen +23, Move Silently +19, Sense Motive +19, Spot +23, Use Rope +4 (+6 with bindings)
-Knowledge (psionics) +12, Speak Language (ninjish), Jump +1
-Hide +15, Move Silently +7, Listen +6, Spot +2
-Hide +1000, Listen +5, Spot +5
-Hide +9, Intimidate +12, Knowledge (psionics) +12, Listen +14, Psicraft +12, Search +12, Sense Motive +12, Spot +14* (comma, comma, ninjas)
-Concentration +17, Hide +7, Jump +16, Knowledge (arcana) +12, Knowledge (psionics) +12, Knowledge (the planes) +12, Listen +22, Move Silently +11, Psicraft +12, Search +12, Sense Motive +14, Spot +22
-Concentration +14, Diplomacy +17, Jump +0, Knowledge (any two) +15, Listen +16, Search +15, Sense Motive +16, Spellcraft +15 (+17 scrolls), Spot +16, Survival +4 (+6 following tracks), Tumble +15, Use Magic Device +15 (+17 scrolls)
-Bluff + 15, Concentration +11 (+15 when manifesting defensively), Hide +14, Listen +14, Move Silently +16
-Jump +16 (or as controlling spirit)
-Climb +38, Knowledge (psionics, arcane) +31, Listen +30, Psicraft +31, Spot +30
-Climb +38 (+39 on a good day)*
-Listen +11, Move Silently +7, Spot +11
-Climb +14*, Listen +6, Move Silently +6, Search +2, Spot +6
-Hide +22, Listen +7, Sense Motive +7, Spot +7
-Climb +12, Jump +20, Listen +7, Spot +8
-Listen +10
-Bluff +10*, Diplomacy +6, Disguise +10*, Intimidate +6, Listen +6, Sense Motive +6, Spot +6
-Climb and Skip +2, Jump +2
-""".splitlines()) # }}}
-
-if __name__ == '__main__':
-    for t in tests:
-        print t
-        parsed = parseSkills(t)
-        print parsed
