6699e432e1261904728cad6797fbfcdf80a3c8ed
Cory Dodt <corymercurial@spam.goonmill.org>
2009-08-21 18:25:09 -0700

    tests for specialparser

--- playtools/test/test_specialparser.py
+++ playtools/test/test_specialparser.py
@@ -39,25 +39,69 @@ class SpecialParserTest(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(specialparser.parseSpecialQualities("-"), [])
 
-    def test_regular(self):
+    def test_immunity(self):
         """
-        Plain jane Specials can parse
+        Immunity Specials can parse
+        """
+        self.compare("Immunity to sleep and paralysis, electricity immunity",
+                "kw=what:sleep type=immunity name=Immunity\n"
+                "kw=what:paralysis type=immunity name=Immunity\n"
+                "kw=what:electricity type=immunity name=Immunity")
+
+    def test_unknown(self):
+        """
+        Unknowns can be captured, and their arguments are also captured
+        """
+        self.compare("Distraction", "kw= type=unknown name=Distraction")
+        self.compare("Distraction (dc 45)", "kw=dc:45 type=unknown name=Distraction")
+
+    def test_misc(self):
+        """
+        Misc Specials can parse
         """
         self.compare("alternate form", "kw= type=noArgumentQuality name=alternate form")
-        self.compare("All-around vision, sonic blast", "kw= type=sense name=All-around vision\nkw= type=unknown name=sonic blast")
-    test_regular.todo = "many more expressions to check still"
+        self.compare("All-around vision, sonic blast", 
+                "kw= type=sense name=All-around vision\n"
+                "kw= type=unknown name=sonic blast")
+        self.compare("Frightful presence", "kw= type=aura name=Frightful presence")
+        self.compare("Frightful presence (dc 20)", "kw=dc:20 type=aura name=Frightful presence")
+        self.compare("Spells", "kw= type=spells name=spells")
+        self.compare("Spell-like abilities, spells (caster level 10)", 
+                "kw= type=noArgumentQuality name=Spell-like abilities\n"
+                "kw=level:10 type=spells name=spells")
+
+    def test_breathWeapon(self):
+        """
+        Breath weapons get parsed
+        """
+        self.compare("Breath weapon", "kw= type=damaging name=Breath weapon")
+        self.compare("Breath weapon (70 ft. cone of fire 24d10, DC 41), breath weapon (70 ft. cone of force 35d12, dc 50)", 
+                "kw=damage:24d10/dc:41/effect:cone of fire/range:70 ft. type=damaging name=Breath weapon\n"
+                "kw=damage:35d12/dc:50/effect:cone of force/range:70 ft. type=damaging name=Breath weapon")
+        self.compare("Breath weapon (70 ft. prismatic spray effect, Dc 41)",
+                "kw=dc:41/effect:prismatic spray effect/range:70 ft. type=damaging name=Breath weapon")
 
     def test_damaging(self):
         """
         Specials that cause damage can parse
         """
+        self.compare("trample", "kw= type=damaging name=trample")
+        self.compare("trample, triple damage",
+            "kw= type=damaging name=trample\n"
+            "kw= type=unknown name=triple damage")
         self.compare("Rend 4d6+18", "kw=damage:4d6+18 type=damaging name=Rend")
         self.compare("Crush 2d8+13 (dc 26)", "kw=damage:2d8+13/dc:26 type=damaging name=Crush")
         self.compare("Crush 2d8+13 plus 1d6 ninjas (dc 26)",
                 "kw=damage:2d8+13/dc:26/extraDamage:plus 1d6 ninjas type=damaging name=Crush")
         self.compare("Constrict 1d1+1 (arm), Constrict 2d2+2 (leg)",
-                """kw=damage:1d1+1/qualifier:arm type=damaging name=Constrict
-kw=damage:2d2+2/qualifier:leg type=damaging name=Constrict""")
+                "kw=damage:1d1+1/qualifier:arm type=damaging name=Constrict\n"
+                "kw=damage:2d2+2/qualifier:leg type=damaging name=Constrict")
+
+    def test_summon(self):
+        """
+        Summon monsters
+        """
+        self.compare("Summon lesser demon of truth", "kw=what:lesser demon of truth type=summon name=Summon")
 
 
 class HUGESpecialParserTest(unittest.TestCase):
