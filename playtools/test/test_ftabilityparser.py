9c654dfa5d60d1638bacc82788e00c9579cfe1c7
Cory Dodt <corymercurial@spam.goonmill.org>
2009-10-25 17:13:47 -0700

    tests for utilities tied into parsing full abilities

--- playtools/test/test_ftabilityparser.py
+++ playtools/test/test_ftabilityparser.py
@@ -4,157 +4,169 @@ descriptions of a monster's special abilities.
 """
 import sys
 import traceback
+import inspect
 from xml.dom import minidom
 
 from twisted.trial import unittest
 
-from playtools.parser import ftabilityparser
-from playtools import fact
-from playtools.util import RESOURCE
+from playtools.parser import ftabilityparser as ft
+from playtools import fact, util
 
 SRD = fact.systems['D20 SRD']
 MONSTERS = SRD.facts['monster']
-
-class FakeElement(object):
-    """
-    Fudge doesn't allow you to .provide an __eq__ because of the unusual
-    circumstances involved in calling __eq__ (apparently it's called via the
-    class rather than the instance).
-    """
-    def __eq__(self, other):
-        return True
-
 
 class FullTextAbilityParserTest(unittest.TestCase):
     """
     Test nuances of parsing
     """
-    def test_flatten(self):
-        fakeEl = FakeElement()
-
-        s = u"""<html foo='bar'/>"""
-        doc = minidom.parseString(s)
-        actual = ftabilityparser.flatten(doc)
-        expected = [(u'html', {u'foo':u'bar'}, fakeEl, [])]
-        self.assertEqual(actual, expected)
-
-        s = u"""<html><head /> stuff </html>"""
-        doc = minidom.parseString(s)
-        actual = ftabilityparser.flatten(doc)
-        expected = [(u'html', {}, fakeEl, 
-            [(u'head', {}, fakeEl, []),
-             (u'#text', {}, u' stuff ', [])])
-            ]
-        self.assertEqual(actual, expected)
-
-        s = u"""<html><head><title>hello</title><meta /></head><body><div
-        la="1">text <p topic="xyz"><b>title</b> other</p>
-        stuff</div><div la="2">other</div></body></html>"""
-        doc = minidom.parseString(s)
-        actual = ftabilityparser.flatten(doc)
-        expected = [
-                (u'html', {}, fakeEl, [
-                    (u'head', {}, fakeEl, [
-                        (u'title', {}, fakeEl, [
-                            ("#text", {}, u'hello', [])]),
-                        (u'meta', {}, fakeEl, [])]),
-                    (u'body', {}, fakeEl, [
-                        (u'div', {u'la':u'1'}, fakeEl, [
-                            ('#text', {}, u'text ', []),
-                            (u'p', {u'topic':u'xyz'}, fakeEl, [
-                                (u'b', {}, fakeEl, [
-                                    ('#text', {}, u'title', [])]),
-                                ('#text', {}, u' other', [])]),
-                            ('#text', {}, u'\n        stuff', [])]),
-                        (u'div', {u'la':u'2'}, fakeEl, [
-                            ('#text', {}, u'other', []) 
-                            ])])
-                        ])
-                    ]
-        self.assertEqual(actual, expected)
-
-    def test_unescape(self):
+    def test_thisLevel(self):
         """
-        Check utility fn to remove slashes removes them.
+        We can correctly identify the level of an sla node - whether the
+        current node is inside a spell, inside a frequencyGroup or at the top
+        level, outside all frequency groups.
         """
-        t1 = r"\n<div topic=\"Atropal\" level=\"3\"><p><h3>Atropal</h3></p><table width=\"100%\" border=\"1\" cellpadding=\"2\" cellspacing=\"2\" frame=\"VOID\" rules=\"ROWS\">"
-        self.assertEqual(ftabilityparser.unescape(t1),
-              "\n<div topic=\"Atropal\" level=\"3\"><p><h3>Atropal</h3></p><table width=\"100%\" border=\"1\" cellpadding=\"2\" cellspacing=\"2\" frame=\"VOID\" rules=\"ROWS\">"
-              )
-
-    def test_prepFT(self):
-        """
-        Check utility function to prep by wrapping in html does it right.
-        """
-        t1 = r"<div foo=\"bar\" />"
-        self.assertEqual(ftabilityparser.prepFT(t1), "<html><div foo=\"bar\" /></html>")
-
-    def test_regular(self):
-        """
-        Plain jane fulls can parse
-        """
-        aranea = open(RESOURCE('plugins/monster/monstertext/aranea.htm')).read()
-        specAbs = ftabilityparser.parseFTAbilities(aranea, prep=0)
-        poison = specAbs[0]
-        self.assertEqual(poison.name, "Poison")
-        self.assertEqual(poison.useCategory, "Ex")
-        self.assertEqual(poison.text, 
-                u'<div level="8" topic="Poison">\n<p> Injury, Fortitude DC 13, initial damage 1d6 Str, secondary damage 2d6 Str. The save DC is Constitution-based.</p>\n</div>'
-                )
-
-    def formatSpellLike(self, sla):
-        return "{0.name}:{0.frequency}:{0.basis}:{0.dc}:{0.casterLevel}:{0.qualifier}".format(sla)
-
-    def test_spellLike(self):
-        """
-        Monsters with spell-like abilities can parse
-        """
-        def getSpellLikes(filename):
-            """
-            Get a list of spell-like abilities by parsing the filename
-            """
-            monster = open(filename).read()
-            specAbs = ftabilityparser.parseFTAbilities(monster, prep=0)
-            ret = []
-            for ab in specAbs:
-                if ab.useCategory == 'Sp':
-                    ret.append(self.formatSpellLike(ab))
-            return ret
-
-        asserted = getSpellLikes(RESOURCE('plugins/monster/monstertext/pixie.htm'))
-        stringy = '\n'.join(map(self.formatSpellLike, asserted))
-        self.assertEqual(stringy, """\
-lesser confusion:1/day:Charisma:14:8:
-dancing lights:1/day:Charisma::8:
-detect chaos:1/day:Charisma::8:
-detect good:1/day:Charisma::8:
-detect evil:1/day:Charisma::8:
-detect law:1/day:Charisma::8:
-detect thoughts:1/day:Charisma:15:8:
-dispel magic:1/day:Charisma::8:
-entangle:1/day:Charisma:14:8:
-permanent image:1/day:Charisma:19:8:visual and auditory elements only
-polymorph:1/day:Charisma::8:self only
-:::::<p>One pixie in ten can use <i>irresistible dance</i> (caster level 8th) once per day.</p>""")
-
-        asserted = getSpellLikes(RESOURCE('plugins/monster/monstertext/dreamLarva.htm')).read()
-        stringy = '\n'.join(map(self.formatSpellLike, asserted))
-        self.assertEqual(stringy, """\
-fly:At will:Charisma:23 + spell level:31:
-haste:At will:Charisma:23 + spell level:31:
-nightmare:At will:Charisma:23 + spell level:31:
-prismatic spray:At will:Charisma:23 + spell level:31:
-dreamscape:2/day:Charisma:23 + spell level:31:""")
-        #
+        s = inspect.cleandoc('''<div xmlns:p='http://' topic="Spell-Like Abilities" id="sla">
+        <span id="1" />
+        <span p:property="frequencyGroup" content="1/day" id="fg">
+            <span id="2" />
+            <span p:property="spell" id="sp">
+                <span content="21" p:property="dc">(DC 21)</span><span content="10" p:property="casterLevel">(caster level 10)</span>
+                <span id="3" />
+            </span>
+            <span p:property="spell">
+                <span id="4" />
+                <span p:property="qualifier">(this is it)</span>
+                <span p:property="qualifier">(this is it too)</span>
+            </span>
+            <span content="19" p:property="dc">(DC 19)</span><span content="8" p:property="casterLevel">(caster level 8)</span>
+        </span>
+        <span content="charisma" p:property="saveDCBasis">The save DCs are Charisma-based</span>
+        </div>''')
+        n = minidom.parseString(s).documentElement
+        t1 = util.findNodeByAttribute(n, 'id', '1')
+        t2 = util.findNodeByAttribute(n, 'id', '2')
+        t3 = util.findNodeByAttribute(n, 'id', '3')
+        t4 = util.findNodeByAttribute(n, 'id', '4')
+        tsla = util.findNodeByAttribute(n, 'id', 'sla')
+        tfg = util.findNodeByAttribute(n, 'id', 'fg')
+        tsp = util.findNodeByAttribute(n, 'id', 'sp')
+        self.assertEqual(ft.thisLevelProperties(t1),
+                {'basis': u'charisma',
+                 'dc': None,
+                 'casterLevel': None,
+                 'frequency': None,
+                 'qualifier': [],
+                    })
+        self.assertEqual(ft.thisLevelProperties(t2),
+                {'basis': u'charisma',
+                 'dc': u'19',
+                 'casterLevel': u'8',
+                 'frequency': u'1/day',
+                 'qualifier': [],
+                    })
+        self.assertEqual(ft.thisLevelProperties(t3),
+                {'basis': u'charisma',
+                 'dc': u'21',
+                 'casterLevel': u'10',
+                 'frequency': u'1/day',
+                 'qualifier': [],
+                    })
+        self.assertEqual(ft.thisLevelProperties(t4),
+                {'basis': u'charisma',
+                 'dc': u'19',
+                 'casterLevel': u'8',
+                 'frequency': u'1/day',
+                 'qualifier': [u'(this is it)', u'(this is it too)'],
+                    })
+        self.assertEqual(ft.thisLevelProperties(tsla),
+                {'basis': u'charisma',
+                 'qualifier': [],
+                 'dc': None,
+                 'casterLevel': None,
+                 'frequency': None,
+                    })
+        self.assertEqual(ft.thisLevelProperties(tfg),
+                {'basis': u'charisma',
+                 'dc': u'19',
+                 'casterLevel': u'8',
+                 'frequency': u'1/day',
+                 'qualifier': [],
+                    })
+        self.assertEqual(ft.thisLevelProperties(tsp),
+                {'basis': u'charisma',
+                 'dc': u'21',
+                 'casterLevel': u'10',
+                 'frequency': u'1/day',
+                 'qualifier': [],
+                    })
 
     def test_null(self):
         """
-        Null fulls parse ok
+        Monsters with no ft abilities parse as empty
         """
-        t = ""
-        parsed = ftabilityparser.parseFTAbilities(t)
-        expected = []
-        self.assertEqual(parsed, expected)
+        self.assertEqual(ft.parseFTAbilities("<html />"), [])
+        t2 = inspect.cleandoc("""<html>
+            <head>
+            <title>water mephit</title>
+            </head>
+            <body>
+            <div id="waterMephit" level="3" topic="Water Mephit">
+            <p/>
+            <h3>Water Mephit</h3>
+            <p>Water mephits speak Common and Aquan.</p>
+            <div level="5" topic="Combat">
+            <p/>
+            <h5>Combat</h5>
+            </div>
+            </div>
+            </body>
+            </html>""")
+        self.assertEqual(ft.parseFTAbilities(t2), [])
+
+    def test_basic(self):
+        """
+        Simple special abilities can be found
+        """
+        t1 = inspect.cleandoc("""<html>
+            <head>
+            <title>water mephit</title>
+            </head>
+            <body>
+            <div id="waterMephit" level="3" topic="Water Mephit">
+            <p/>
+            <h3>Water Mephit</h3>
+            <p>Water mephits speak Common and Aquan.</p>
+            <div level="5" topic="Combat">
+            <p/>
+            <h5>Combat</h5>
+            <div level="8" topic="Breath Weapon">
+            <p><b>Breath Weapon (Su):</b> 15-foot cone of caustic liquid, damage 1d8 acid, Reflex DC 13 half. The save DC is Constitution-based and includes a +1 racial bonus.</p>
+            </div>
+            </div>
+            </div>
+            </body>
+            </html>""")
+        actual = ft.parseFTAbilities(t1)
+        self.assertEqual(map(repr, actual), 
+                ["Power:Breath Weapon|Su|None|None|None|None|None"])
+
+    def test_spellLike(self):
+        t = inspect.cleandoc("""<div level="5" topic="Combat" xmlns:p="http://test">
+            <p/>
+            <h5>Combat</h5>
+            <div level="8" topic="Spell-Like Abilities"> <p><b p:property="powerName">Spell-Like Abilities:</b> <span content="1/hour" p:property="frequencyGroup">1/hour-<span content="acid arrow" p:property="spell"><i p:property="spellName">acid arrow</i> <span p:property="qualifier">(can hurl an acidic
+              blob that functions like the spell)</span> <span content="3" p:property="casterLevel">(caster level 3)</span></span></span>; <span content="1/day" p:property="frequencyGroup">1/day-<span content="stinking
+                cloud" p:property="spell"><i p:property="spellName">stinking
+                cloud</i> <span p:property="qualifier">(a mass of smelly fog that duplicates the effect of the spell)</span> <span content="15" p:property="dc">(DC 15)</span><span content="6" p:property="casterLevel">(caster level 6)</span></span></span>.<span content="charisma" p:property="saveDCBasis">The save DCs are Charisma-based</span>.</p> </div>
+            </div>""")
+        actual = ft.parseFTAbilities(t)
+        self.assertEqual(map(repr, actual),
+                ["Power:acid arrow|Sp|1/hour|Charisma|None|3|"
+                 "(can hurl an acidic blob that functions like the spell)",
+
+                 "Power:stinking cloud|Sp|1/day|Charisma|15|6|"
+                 "(a mass of smelly fog that duplicates the effect of the spell)",
+                 ])
 
 
 class HUGEFullTextAbilityParserTest(unittest.TestCase):
@@ -171,7 +183,7 @@ class HUGEFullTextAbilityParserTest(unittest.TestCase):
 
             try:
                 if stat1:
-                    act = [monster.name, ftabilityparser.parseFTAbilities(stat1) and None]
+                    act = [monster.name, ft.parseFTAbilities(stat1) and None]
             except:
                 f = traceback.format_exc(sys.exc_info()[2])
                 self.assertTrue(False,
